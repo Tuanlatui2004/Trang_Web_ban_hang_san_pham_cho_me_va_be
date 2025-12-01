308e1fbb8e17efb41a2b015cb43e93aef63bd013
PhamBaHaiAnh <23130012@st.hcmuaf.edu.vn>
2025-12-01 21:26:59 +0700

    bảng card, categories, image, option_variant_value, option_variant

--- database/E_Commerce_Mom_and_Baby_Store.sql
+++ database/E_Commerce_Mom_and_Baby_Store.sql
@@ -59,3 +59,84 @@ CREATE TABLE IF NOT EXISTS `brands` (
     `name` varchar(191) default null,
     PRIMARY KEY (`brand_id`)
     ) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+--
+--  Table structure for table `cart_items`
+--
+
+
+DROP TABLE IF EXISTS `card`;
+CREATE TABLE `card` (
+                        `id` int(11) NOT NULL AUTO_INCREMENT,
+                        `user_id` int(11) DEFAULT NULL,
+                        `duration` date DEFAULT NULL,
+                        `type` varchar(50) DEFAULT NULL,
+                        `is_default` tinyint(4) DEFAULT NULL,
+                        PRIMARY KEY (`card_id`),
+                        KEY `user_id` (`user_id`),
+                        CONSTRAINT `fk_card` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+--  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+--
+--  Table structure for table `categories`
+--
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+                              `id` int(11) NOT NULL AUTO_INCREMENT,
+                              `name` varchar(255) NOT NULL,
+                              PRIMARY KEY (`category_id`),
+                              UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+
+
+--  -- -- -- -- -- -- -- -- -- -- -- -- -- --
+--  Table structure for image
+--  -- -- -- -- -- -- -- -- -- -- -- -- -- --
+DROP TABLE IF EXISTS `image`;
+CREATE TABLE `image` (
+                         `id` int(11) NOT NULL AUTO_INCREMENT,
+                         `url` text NOT NULL,
+                         PRIMARY KEY (`image_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+--
+
+--
+
+--  -- -- -- -- -- -- -- -- -- -- -- -- -- --
+--  Table structure for option_variant_value
+--  -- -- -- -- -- -- -- -- -- -- -- -- -- --
+DROP TABLE IF EXISTS `option_variant_value`;
+CREATE TABLE `option_variant_value` (
+                                        `id` int(11) NOT NULL AUTO_INCREMENT,
+                                        `option_id` int(11) DEFAULT NULL,
+                                        `variant_id` int(11) DEFAULT NULL,
+                                        PRIMARY KEY (`option_variant_value_id`),
+                                        KEY `fk_option_variant_value_1` (`option_id`),
+                                        KEY `fk_option_variant_value_2` (`variant_value_id`),
+                                        CONSTRAINT `fk_option_variant_value_1` FOREIGN KEY (`option_id`) REFERENCES `option_variant` (`id`),
+                                        CONSTRAINT `fk_option_variant_value_2` FOREIGN KEY (`variant_id`) REFERENCES `variant_value` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+--  -- -- -- -- -- -- -- -- -- -- -- -- -- --
+--  Table structure for option_variant
+--  -- -- -- -- -- -- -- -- -- -- -- -- -- --
+DROP TABLE IF EXISTS `option_variant`;
+CREATE TABLE `option_variant` (
+                                  `id` int(11) NOT NULL AUTO_INCREMENT,
+                                  `product_id` int(11) DEFAULT NULL,
+                                  `price` int(11) DEFAULT NULL,
+                                  `stock` int(11) DEFAULT NULL,
+                                  PRIMARY KEY (`option_id`),
+                                  KEY `option_variant_fk_1` (`product_id`),
+                                  CONSTRAINT `option_variant_fk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT    CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
