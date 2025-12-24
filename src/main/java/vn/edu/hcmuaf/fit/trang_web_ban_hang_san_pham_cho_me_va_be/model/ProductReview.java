0cf4e43b79464b9432b9d01c53e2d8c6aaacd3e5
dev-vinh <23130385@st.hcmuaf.edu.vn>
2025-12-24 12:10:46 +0700

    xog phần models theo db

--- src/main/java/vn/edu/hcmuaf/fit/trang_web_ban_hang_san_pham_cho_me_va_be/model/ProductReview.java
+++ src/main/java/vn/edu/hcmuaf/fit/trang_web_ban_hang_san_pham_cho_me_va_be/model/ProductReview.java
@@ -1,54 +1,66 @@
 package vn.edu.hcmuaf.fit.trang_web_ban_hang_san_pham_cho_me_va_be.model;
 
-import java.io.Serializable;
+import jakarta.annotation.Nullable;
+import org.jdbi.v3.core.mapper.reflect.ColumnName;
+import org.jdbi.v3.core.mapper.reflect.JdbiConstructor;
 
-public class ProductReview implements Serializable {
-    private int id;
-    private int user_id;
-    private int product_id;
-    private int rating;
-    private String description;
+public class ProductReview{
+    Integer id;
+    Integer user_id;
+    Integer product_id;
+    Integer rating;
+    String description;
+    Integer order_id;
 
-    public ProductReview(int id, int user_id, int product_id, int rating, String description) {
+
+    @JdbiConstructor
+    public ProductReview(@ColumnName("id") @Nullable Integer id,
+                  @ColumnName("user_id") @Nullable Integer user_id,
+                  @ColumnName("product_id") @Nullable Integer product_id,
+                  @ColumnName("rating")@Nullable Integer rating,
+                  @ColumnName("description") @Nullable String description,
+                  @ColumnName("order_id") @Nullable Integer order_id)
+    {
         this.id = id;
         this.user_id = user_id;
         this.product_id = product_id;
         this.rating = rating;
         this.description = description;
+        this.order_id = order_id;
     }
 
     public ProductReview() {
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
-    public int getUser_id() {
+    public Integer getUser_id() {
         return user_id;
     }
 
-    public void setUser_id(int user_id) {
+    public void setUser_id(Integer user_id) {
         this.user_id = user_id;
     }
 
-    public int getProduct_id() {
+    public Integer getProduct_id() {
         return product_id;
     }
 
-    public void setProduct_id(int product_id) {
+    public void setProduct_id(Integer product_id) {
         this.product_id = product_id;
     }
 
-    public int getRating() {
+    public Integer getRating() {
         return rating;
     }
 
-    public void setRating(int rating) {
+    public void setRating(Integer rating) {
         this.rating = rating;
     }
 
@@ -58,5 +70,25 @@ public class ProductReview implements Serializable {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public Integer getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(Integer order_id) {
+        this.order_id = order_id;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductReview{" +
+                "id=" + id +
+                ", user_id=" + user_id +
+                ", product_id=" + product_id +
+                ", rating=" + rating +
+                ", description='" + description + '\'' +
+                ", order_id=" + order_id +
+                '}';
     }
 }
--- src/main/java/vn/edu/hcmuaf/fit/trang_web_ban_hang_san_pham_cho_me_va_be/model/User.java
+++ src/main/java/vn/edu/hcmuaf/fit/trang_web_ban_hang_san_pham_cho_me_va_be/model/User.java
@@ -1,110 +1,80 @@
 package vn.edu.hcmuaf.fit.trang_web_ban_hang_san_pham_cho_me_va_be.model;
 
-import java.io.Serializable;
-import java.sql.Date;
+import jakarta.annotation.Nullable;
+import org.jdbi.v3.core.mapper.reflect.ColumnName;
+import org.jdbi.v3.core.mapper.reflect.JdbiConstructor;
 
-public class User implements Serializable {
-    private int id;
-    private String fullName;
-    private String displayName;
-    private Date dOB;
-    private String gender;
-    private String phone_number;
-    private String email;
-    private String password_Username;
-    private int avatar_id;
-    private String status;
-    private String role;
-    private String confirmationToken;
+import java.time.LocalDate;
 
-    public User(int id, String fullName, String displayName, Date dOB, String gender, String phone_number, String email, String password_Username, int avatar_id, String status, String role, String confirmationToken) {
-        this.id = id;
-        this.fullName = fullName;
-        this.displayName = displayName;
-        this.dOB = dOB;
-        this.gender = gender;
-        this.phone_number = phone_number;
-        this.email = email;
-        this.password_Username = password_Username;
-        this.avatar_id = avatar_id;
-        this.status = status;
-        this.role = role;
-        this.confirmationToken = confirmationToken;
+public class User {
+    Integer id;
+    String fullName;
+    String displayName;
+    LocalDate dOB;
+    String gender;
+    String phone_number;
+    String email;
+    String password_Username;
+    Integer avatar_id;
+    String status;
+    Role role;
+    String confirmationToken;
+
+
+    // xem
+
+//    String facebookId;
+//    Boolean needRefresh;
+//    String salt;
+//    String avatarUrl;
+
+    @JdbiConstructor
+    public User(@ColumnName("id") Integer id,
+                @ColumnName("fullName") @Nullable  String fullName,
+                @ColumnName("displayName") @Nullable String displayName,
+                @ColumnName("dOB") @Nullable LocalDate  dOB,
+                @ColumnName("gender") @Nullable String gender,
+                @ColumnName("phone") @Nullable  String phone_number,
+                @ColumnName("email")@Nullable String email,
+                @ColumnName("password") @Nullable String password_Username,
+                @ColumnName("avatarId") @Nullable Integer avatar_id,
+                @ColumnName("status") @Nullable String status,
+                @ColumnName("role") @Nullable Role role,
+                @ColumnName("confirmationToken") @Nullable String confirmationToken
+
+    ){
+    this.id=id;
+    this.fullName=fullName;
+    this.displayName=displayName;
+    this.dOB=dOB;
+    this.gender=gender;
+    this.phone_number=phone_number;
+    this.email=email;
+    this.password_Username=password_Username;
+    this.avatar_id=avatar_id;
+    this.status=status;
+    this.role=role;
+    this.confirmationToken=confirmationToken;
+
     }
 
     public User() {
     }
 
-    public int getId() {
-        return id;
+    public String getConfirmationToken() {
+        return confirmationToken;
     }
 
-    public void setId(int id) {
-        this.id = id;
+    public void setConfirmationToken(String confirmationToken) {
+        this.confirmationToken = confirmationToken;
     }
 
-    public String getFullName() {
-        return fullName;
+    public Role getRole() {
+        return role;
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
-    }
-
-    public Date getdOB() {
-        return dOB;
-    }
-
-    public void setdOB(Date dOB) {
-        this.dOB = dOB;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getPhone_number() {
-        return phone_number;
-    }
-
-    public void setPhone_number(String phone_number) {
-        this.phone_number = phone_number;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword_Username() {
-        return password_Username;
-    }
-
-    public void setPassword_Username(String password_Username) {
-        this.password_Username = password_Username;
-    }
-
-    public int getAvatar_id() {
-        return avatar_id;
-    }
-
-    public void setAvatar_id(int avatar_id) {
-        this.avatar_id = avatar_id;
+    public void setRole(Role role) {
+        this.role = role;
     }
 
     public String getStatus() {
@@ -115,19 +85,93 @@ public class User implements Serializable {
         this.status = status;
     }
 
-    public String getRole() {
-        return role;
+    public Integer getAvatar_id() {
+        return avatar_id;
     }
 
-    public void setRole(String role) {
-        this.role = role;
+    public void setAvatar_id(Integer avatar_id) {
+        this.avatar_id = avatar_id;
     }
 
-    public String getConfirmationToken() {
-        return confirmationToken;
+    public String getPassword_Username() {
+        return password_Username;
     }
 
-    public void setConfirmationToken(String confirmationToken) {
-        this.confirmationToken = confirmationToken;
+    public void setPassword_Username(String password_Username) {
+        this.password_Username = password_Username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getdOB() {
+        return dOB;
+    }
+
+    public void setdOB(LocalDate dOB) {
+        this.dOB = dOB;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", displayName='" + displayName + '\'' +
+                ", dOB=" + dOB +
+                ", gender='" + gender + '\'' +
+                ", phone_number='" + phone_number + '\'' +
+                ", email='" + email + '\'' +
+                ", password_Username='" + password_Username + '\'' +
+                ", avatar_id=" + avatar_id +
+                ", status='" + status + '\'' +
+                ", role=" + role +
+                ", confirmationToken='" + confirmationToken + '\'' +
+                '}';
     }
 }
