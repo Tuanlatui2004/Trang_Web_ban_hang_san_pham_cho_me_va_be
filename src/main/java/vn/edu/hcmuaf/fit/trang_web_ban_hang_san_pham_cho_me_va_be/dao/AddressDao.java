266110f2dec2b258864fe738d055f74ed49a387f
PhamBaHaiAnh <23130012@st.hcmuaf.edu.vn>
2026-01-01 22:27:29 +0700

    fix các phương thưc lỗi ở AddressDao

--- src/main/java/vn/edu/hcmuaf/fit/trang_web_ban_hang_san_pham_cho_me_va_be/dao/AddressDao.java
+++ src/main/java/vn/edu/hcmuaf/fit/trang_web_ban_hang_san_pham_cho_me_va_be/dao/AddressDao.java
@@ -3,6 +3,7 @@ package vn.edu.hcmuaf.fit.trang_web_ban_hang_san_pham_cho_me_va_be.dao;
 import org.jdbi.v3.sqlobject.config.RegisterConstructorMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import vn.edu.hcmuaf.fit.trang_web_ban_hang_san_pham_cho_me_va_be.model.Address;
@@ -22,31 +23,16 @@ public interface AddressDao {
                 " WHERE id = :address_id;")
         Address getAddressById(@Bind("address_id") Integer address_id);
 
-
-
-        @SqlQuery(value = "SELECT *" +
-                " FROM address" +
-                " WHERE user_id = :user_id and is_default =1;")
-        Address getAddressDefaultByUserId(@Bind("user_id") Integer user_id);
-
-
-        @SqlUpdate("UPDATE address " +
-                "SET is_default = :default_status " +
-                "WHERE id = :id; ")
-        Boolean updateDefaultById(@Bind("id") Integer id, @Bind("default_status") boolean default_status);
-
-
-
-
-        @SqlUpdate("INSERT INTO address (user_id, province, district, commune,  detail, phone, name, is_default, type, status) " +
-                "VALUES (:user_id, :province, :district,  :commune, :detail, :phone, :name, :is_default, :type, :status)")
-        int addAddress(    @BindBean Address address);
-
-
-
-        @SqlUpdate("UPDATE address " +
-                " SET status=:status " +
-                "WHERE id =:id ")
-        Boolean updateStatus(@Bind("id") Integer id,@Bind("status") String status);
-
+    @SqlUpdate("INSERT INTO address (user_id, province, district, commune, detail, phone, name, is_default, type) " +
+            "VALUES (:user_id, :province, :district, :commune, :detail, :phone, :name, :is_default, :type)")
+    @GetGeneratedKeys("id")
+    int addAddress(@Bind("userId") Integer user_id,
+                   @Bind("province") String province,
+                   @Bind("district") String district,
+                   @Bind("commune") String commune,
+                   @Bind("detail") String detail,
+                   @Bind("phone") String phone,
+                   @Bind("name") String name,
+                   @Bind("isDefault") Boolean is_default,
+                   @Bind("type") String type);
 }
