21de6659c074671a8d99d26e6a01ae7ca99ae415
PhamBaHaiAnh <23130012@st.hcmuaf.edu.vn>
2026-01-10 12:30:35 +0700

    do no thing

--- src/main/java/vn/edu/hcmuaf/fit/trang_web_ban_hang_san_pham_cho_me_va_be/controller/product/ProductDetailController.java
+++ src/main/java/vn/edu/hcmuaf/fit/trang_web_ban_hang_san_pham_cho_me_va_be/controller/product/ProductDetailController.java
@@ -24,16 +24,7 @@ public class ProductDetailController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String idParam = request.getParameter("id");
-
-        if (idParam == null || idParam.isEmpty()) {
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing product id");
-            return;
-        }
-
-        int id = Integer.parseInt(idParam);
-
-//        int product_id = Integer.parseInt(request.getParameter("id"));
+        int product_id = Integer.parseInt(request.getParameter("id"));
         Product product = productService.getProductById(product_id);
 
         Integer product_price = productService.getMinimumPriceForProduct(product_id); // Default to minimum price
